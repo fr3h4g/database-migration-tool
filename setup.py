07008b3398254dc59605734419dc11144934570e
Fredrik Haglund <fredrik@petitcircuitlab.com>
2022-08-08 20:18:32 +0200

    added more classifiers

--- setup.py
+++ setup.py
@@ -34,6 +34,10 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
