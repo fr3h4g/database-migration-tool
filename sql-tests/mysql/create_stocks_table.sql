f43c5b7cb0ceb44c537bdde7a69e091ecdfcb4d2
Fredrik Haglund <fredrik@petitcircuitlab.com>
2022-08-09 11:34:15 +0200

    fixed sql, disable check

--- sql-tests/mysql/create_stocks_table.sql
+++ sql-tests/mysql/create_stocks_table.sql
@@ -3,5 +3,5 @@ CREATE TABLE `stocks` (
     `trans` VARCHAR(50) NOT NULL , 
     `symbol` VARCHAR(50) NOT NULL , 
     `qty` VARCHAR(50) NOT NULL , 
-    `price` VARCHAR(50) NOT NULL , 
+    `price` VARCHAR(50) NOT NULL
 ) ENGINE = InnoDB;
